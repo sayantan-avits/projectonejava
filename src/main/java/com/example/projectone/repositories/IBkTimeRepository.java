39f6359f31210f10c777b18e471396f88ed3c9e7
Sayantan Mondal <sayantanmondal8@gmail.com>
2019-04-10 12:14:20 +0530

    Procfile added

--- src/main/java/com/example/projectone/repositories/IBkTimeRepository.java
+++ src/main/java/com/example/projectone/repositories/IBkTimeRepository.java
@@ -2,10 +2,9 @@ package com.example.projectone.repositories;
 
 import com.example.projectone.model.BkTimeLogModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
+@Repository
 public interface IBkTimeRepository extends JpaRepository<BkTimeLogModel, Long> {
 
 
